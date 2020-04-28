8c79fc664021fe6c54389523853195652c79a39d
Swapnil <swapnilkant11@gmail.com>
2020-04-28 16:17:04 +0530

    deployment

---
+++ config/keys-prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://socialmediaapp:socialmediaapp1@socialmediaapp-yttdr.mongodb.net/test?retryWrites=true&w=majority',
+    GoogleClientID: '332508366717-kk9c3rscqotq5hdu5t29p1ve74u1uhf3.apps.googleusercontent.com',
+    GoogleClientSecret: 'JaJYwQ5ZQ1_JvVjlvKsCwlna'
+}
