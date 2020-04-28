e2e6bd27802d045746582951ffe91ded4bba8ba5
Swapnil <swapnilkant11@gmail.com>
2020-04-28 16:37:01 +0530

    configkeys

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://socialmediaapp:socialmediaapp1@socialmediaapp-yttdr.mongodb.net/test?retryWrites=true&w=majority',
-    GoogleClientID: '332508366717-kk9c3rscqotq5hdu5t29p1ve74u1uhf3.apps.googleusercontent.com',
-    GoogleClientSecret: 'JaJYwQ5ZQ1_JvVjlvKsCwlna'
+    MongoURI: process.env.MONGO_URI,
+    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
